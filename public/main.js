492b84af769e4bd0cd0d35ddaad829d0653370dd
OCPTECH\CristhianZarate <cristhian.zarate@ocp.tech>
2026-02-01 23:27:43 -0500

    Actualizada url de despliegue

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
-//const myServerUrl = 'https://video-customer-support-production.up.railway.app';
-const myServerUrl = 'http://localhost:3000'; // Update this with your server URL
+const myServerUrl = 'https://videocustomer-cisco-production.up.railway.app';
+//const myServerUrl = 'http://localhost:3000'; // Update this with your server URL
 async function getAccessToken() {
     const url = myServerUrl + '/get-access-token';
     const requestOptions = {
